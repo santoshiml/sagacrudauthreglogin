a0d31fc3179137ba232f90427eb70a9853eb42bb
santoshi342 <santoshiyuvasoft342@gmail.com>
2022-07-07 18:52:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
+import React, { Suspense, lazy, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useNavigate,
+  useLocation,
+} from "react-router-dom";
+import Login from "./Components/Login";
+import Register from "./Components/Register";
+import Header from "./Header";
+import Enquiries from "./Components/Dashboard/Enquiries";
+import FAQ from "./Components/Dashboard/FAQ";
+import Profile from "./Components/Dashboard/Profile";
+import PageNotFound from "./Components/PageNotFound";
+
+const Dashboard = lazy(() => import("./Components/Dashboard"));
 
 function App() {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+
+  let token = localStorage.getItem("Token");
+  // console.log("llllll", token);
+
+  useEffect(() => {
+    if (token && pathname == "/") {
+      navigate("/dashboard");
+    } else if (!token && pathname == "/dashboard") {
+      navigate("/");
+    } else {
+      navigate(pathname);
+    }
+  }, [token]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ToastContainer />
+      <Suspense
+        fallback={
+          <div>
+            <Box
+              sx={{
+                display: "flex",
+                height: "100vh",
+                justifyContent: "center",
+              }}
+            >
+              <CircularProgress />
+            </Box>
+          </div>
+        }
+      >
+        <Routes>
+          {token ? (
+            <>
+              <Route path="/dashboard" element={<Dashboard />}></Route>
+              <Route path="/enquiries" element={<Enquiries />} />
+              <Route path="/profile" element={<Profile />} />
+            </>
+          ) : (
+            <>
+              <Route path="/" element={<Register />} />
+              <Route path="/login" element={<Login />} />
+            </>
+          )}
+
+          <Route path="*" exact={true} element={<PageNotFound />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
